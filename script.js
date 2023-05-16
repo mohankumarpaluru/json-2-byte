9851cbc342d981a92ae3e11e2a37999e54dce0c0
mohankumarpaluru <mohankumarpaluru@gmail.com>
2023-05-16 10:17:31 +0530

    Added Icons, Everything is good except CSS

--- script.js
+++ script.js
@@ -1,63 +1,96 @@
-const form = document.querySelector('#json-form');
-const input = document.querySelector('#byte64-input');
-const output = document.querySelector('#json-output');
+const form = document.querySelector("#json-form");
+const input = document.querySelector("#byte64-input");
+const output = document.querySelector("#json-output");
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const encodedJson = input.value.trim();
-  if (!encodedJson) {
-    alert('Please enter a valid Base64 encoded JSON.');
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const base64EncodedString = input.value.trim();
+
+  if (!base64EncodedString) {
+    alert("Please enter a valid Base64 encoded JSON.");
     return;
   }
-  try {
-    const decodedJson = atob(encodedJson);
-    const jsonData = JSON.parse(decodedJson);
 
-    let jsonHtml = '';
-    let keyCount = 0;
-    for (const [key, value] of Object.entries(jsonData)) {
-      jsonHtml += `<div class='keypairinput'>`
-      jsonHtml += `<label for="KEY_${keyCount}">KEY_${keyCount}:</label>`;
-      jsonHtml += `<input type="text" name="KEY_${keyCount}" id="KEY_${keyCount}:" value="${key}">`;
-      jsonHtml += `<label for="VALUE_${keyCount}">VALUE_${keyCount}:</label>`;
-      jsonHtml += `<input type="text" name="VALUE_${keyCount}" id="VALUE_${keyCount}" value="${value}"><br>`;
-      jsonHtml += `</div>`
-      keyCount++;
+  try {
+    const json = JSON.parse(atob(base64EncodedString));
+    let html = "";
+    let id = 0;
+
+    for (const [key, value] of Object.entries(json)) {
+      html += `<div class='keypairinput' id='keypair-${id}'>`;
+      html += `<label for="KEY_${id}">KEY_${id}:</label>`;
+      html += `<input type="text" name="KEY_${id}" id="KEY_${id}:" value="${key}">`;
+      html += `<label for="VALUE_${id}">VALUE_${id}:</label>`;
+      html += `<input type="text" name="VALUE_${id}" id="VALUE_${id}" value="${value}">`;
+      html += `<button type="button" class="remove-btn" data-keypair-id="${id}"><ion-icon name="trash"></ion-icon></button><br>`;
+      html += "</div>";
+      id++;
     }
 
     output.innerHTML = `
       <h2>JSON Editor</h2>
       <form id="json-editor-form">
-        ${jsonHtml}
+        ${html}
         <br>
+        <button type="button" id="add-btn"><ion-icon name="add-circle-sharp"></ion-icon> Add Key-Value Pair</button>
         <input type="submit" value="Encode">
       </form>
     `;
 
-    const editorForm = document.querySelector('#json-editor-form');
-    editorForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-      const newData = {};
-      for (const [key, value] of new FormData(editorForm).entries()) {
-        if (key.startsWith("KEY_")) {
-          var valValue = value;
-          newData[value] = '';
-        }
-        else if(key.startsWith("VALUE_")) {
-          newData[valValue] = value;
+    const editorForm = document.querySelector("#json-editor-form");
+    const addBtn = document.querySelector("#add-btn");
+    let nextId = id;
+
+    addBtn.addEventListener("click", () => {
+      const keyPairInput = document.createElement("div");
+      keyPairInput.classList.add("keypairinput");
+      keyPairInput.id = `keypair-${nextId}`;
+      keyPairInput.innerHTML = `
+        <label for="KEY_${nextId}">KEY_${nextId}:</label>
+        <input type="text" name="KEY_${nextId}" id="KEY_${nextId}:" value="">
+        <label for="VALUE_${nextId}">VALUE_${nextId}:</label>
+        <input type="text" name="VALUE_${nextId}" id="VALUE_${nextId}" value="">
+        <button type="button" class="remove-btn" data-keypair-id="${nextId}"><ion-icon name="trash"></ion-icon></button><br>
+      `;
+
+      editorForm.insertBefore(keyPairInput, addBtn);
+      nextId++;
+    });
+
+    output.addEventListener("click", (e) => {
+      if (e.target.classList.contains("remove-btn")) {
+        const keypairId = e.target.dataset.keypairId;
+        const keypair = document.querySelector(`#keypair-${keypairId}`);
+        keypair.parentNode.removeChild(keypair);
+      }
+    });
+
+    const jsonEditorForm = document.querySelector("#json-editor-form");
+    jsonEditorForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const formData = new FormData(jsonEditorForm);
+      const data = {};
+
+      for (const [name, value] of formData.entries()) {
+        if (name.startsWith("KEY_")) {
+          data[value] = "";
+        } else if (name.startsWith("VALUE_")) {
+          data[data[name.replace("VALUE_", "KEY_")]] = value;
         }
       }
-      const newJsonData = JSON.stringify(newData);
-      const newEncodedJson = btoa(newJsonData);
+
+      const json = JSON.stringify(data);
+      const base64EncodedJson = btoa(json);
 
       output.innerHTML += `
         <div id="output-json">
           <h2>Output Base64 Encoded JSON:</h2>
-          <textarea class="form-control" name="encoded-json" id="encoded-json" cols="50" rows="5" readonly>${newEncodedJson}</textarea><br>
+          <textarea class="form-control" name="encoded-json" id="encoded-json" cols="50" rows="5" readonly>${base64EncodedJson}</textarea><br>
         </div>
       `;
     });
   } catch (error) {
-    alert('Invalid Base64 encoded JSON.');
+    alert("Invalid Base64 encoded JSON.");
   }
 });
